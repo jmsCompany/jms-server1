b1f882ca464d333d0f557bea1fc42ca70dbb8026
Hongtao Ren <ren@ecust.edu.cn>
2015-06-10 07:11:45 +0800

    validator works

--- jms-webservice/jms-webservice-user/src/main/java/com/jms/controller/user/UserController.java
+++ jms-webservice/jms-webservice-user/src/main/java/com/jms/controller/user/UserController.java
@@ -1,11 +1,16 @@
 package com.jms.controller.user;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import com.jms.domain.ws.Message;
 import com.jms.domain.ws.WSRoles;
+import com.jms.domain.ws.WSUser;
 import com.jms.service.user.UserService;
 
 @RestController
@@ -19,6 +24,11 @@ public class UserController {
 		return userService.checkLogin(login);
 	}
 	
-
+	@RequestMapping(value="user/registor", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void regist(@RequestBody @Valid WSUser wsUser, BindingResult bindingResult) throws Exception {
+		//return userService.checkLogin(login);
+		if(bindingResult.hasErrors())
+			System.out.println("error!!!!!!!!!!!!!!!!!!");
+	}
 
 }
