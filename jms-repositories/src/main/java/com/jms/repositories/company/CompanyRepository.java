6612919885eaeb16563bf75697a531c47a1c8c1b
Hongtao REN From Mac <h-t-ren@icloud.com>
2015-12-21 14:21:47 +0800

    new changed

--- jms-repositories/src/main/java/com/jms/repositories/company/CompanyRepository.java
+++ jms-repositories/src/main/java/com/jms/repositories/company/CompanyRepository.java
@@ -1,7 +1,6 @@
 package com.jms.repositories.company;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jms.domain.db.Company;
--- jms-webservice/jms-webservice-system/src/main/java/com/jms/controller/system/DistrictController.java
+++ jms-webservice/jms-webservice-system/src/main/java/com/jms/controller/system/DistrictController.java
@@ -5,10 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import com.jms.domain.ws.WSCity;
-import com.jms.domain.ws.WSDistrict;
-import com.jms.domain.ws.WSProvince;
 import com.jms.domain.ws.WSSelectObj;
 import com.jms.service.system.DistrictService;
 
